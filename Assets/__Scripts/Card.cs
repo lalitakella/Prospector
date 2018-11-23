3a43b01a1c873dda2e5e4f1e1179ddc71e00ed06
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-11-22 19:08:56 -0500

    trouble in deck .cs

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -2,20 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Card : MonoBehaviour {
+public class Card : MonoBehaviour
+{
 
     [Header("Set Dynamically")]
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
-	public List<GameObject> decoGOs = new List<GameObject>();
-	public List<GameObject> pipGOs = new List<GameObject>();
-	
-	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml	
+    public string suit;
+    public int rank;
+    public Color color = Color.black;
+    public string colS = "Black";  // or "Red"
+
+    public List<GameObject> decoGOs = new List<GameObject>();
+    public List<GameObject> pipGOs = new List<GameObject>();
+
+    public GameObject back;  // back of card;
+    public CardDefinition def;  // from DeckXML.xml	
     public SpriteRenderer[] spriteRenderers;
 
     void Start()
@@ -25,7 +26,7 @@ public class Card : MonoBehaviour {
 
     public void PopulateSpriteRenderers()
     {
-        if(spriteRenderers == null || spriteRenderers.Length == 0)
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
         {
             spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
         }
@@ -34,7 +35,7 @@ public class Card : MonoBehaviour {
     public void SetSortingLayerName(string tSLN)
     {
         PopulateSpriteRenderers();
-        foreach(SpriteRenderer tSR in spriteRenderers)
+        foreach (SpriteRenderer tSR in spriteRenderers)
         {
             tSR.sortingLayerName = tSLN;
         }
@@ -43,9 +44,9 @@ public class Card : MonoBehaviour {
     public void SetSortOrder(int sOrd)
     {
         PopulateSpriteRenderers();
-        foreach(SpriteRenderer tSR in spriteRenderers)
+        foreach (SpriteRenderer tSR in spriteRenderers)
         {
-            if(tSR.gameObject == this.gameObject)
+            if (tSR.gameObject == this.gameObject)
             {
                 tSR.sortingOrder = sOrd;
             }
@@ -63,28 +64,34 @@ public class Card : MonoBehaviour {
         }
     }
 
-    public bool faceUp {
-		get {
-			return (!back.activeSelf);
-		}
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
 
-		set {
-			back.SetActive(!value);
-		}
-	}
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
 
-[System.Serializable]
-public class Decorator{
-	public string	type;			// For card pips, tyhpe = "pip"
-	public Vector3	loc;			// location of sprite on the card
-	public bool		flip = false;	//whether to flip vertically
-	public float 	scale = 1.0f;
-}
+    [System.Serializable]
+    public class Decorator
+    {
+        public string type;         // For card pips, tyhpe = "pip"
+        public Vector3 loc;         // location of sprite on the card
+        public bool flip = false;   //whether to flip vertically
+        public float scale = 1.0f;
+    }
 
-[System.Serializable]
-public class CardDefinition{
-	public string	face;	//sprite to use for face cart
-	public int		rank;	// value from 1-13 (Ace-King)
-	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+    [System.Serializable]
+    public class CardDefinition
+    {
+        public string face; //sprite to use for face cart
+        public int rank;    // value from 1-13 (Ace-King)
+        public List<Decorator>
+                        pips = new List<Decorator>();  // Pips Used
+    }
 }
