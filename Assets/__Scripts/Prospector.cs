fa10bf8f284541bfd9d7f39b65f7b3b3a881f7e9
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-11-29 21:15:23 -0500

    replayable

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -201,7 +201,42 @@ public class Prospector : MonoBehaviour
                 MoveToTarget(cd);
                 SetTableauFaces();
                 break;
+                
         }
+        CheckForGameOver();
+    }
+    void CheckForGameOver()
+    {
+        if(tableau.Count == 0)
+        {
+            GameOver(true);
+            return;
+        }
+        if (drawPile.Count > 0)
+        {
+            return;
+        }
+        foreach(CardProspector cd in tableau)
+        {
+            if(AdjacentRank(cd, target))
+            {
+                return;
+            }
+        }
+        GameOver(false);
+    }
+
+    void GameOver(bool won)
+    {
+        if (won)
+        {
+            print("Game Over. You Won! :)");
+        }
+        else
+        {
+            print("Game Over. You Lost. :(");
+        }
+        SceneManager.LoadScene("_Prospector_Scene_0");
     }
     public bool AdjacentRank(CardProspector c0, CardProspector c1)
     {
