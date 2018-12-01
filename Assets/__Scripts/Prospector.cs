e2dcdc35f3dbf2c03721839790e75162b6f2996c
lalitakella <31657061+lalitakella@users.noreply.github.com>
2018-12-01 17:25:38 -0500

    SceneManager

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -184,6 +184,7 @@ public class Prospector : MonoBehaviour
                 MoveToDiscard(target);
                 MoveToTarget(Draw());
                 UpdateDrawPile();
+                ScoreManager.EVENT(eScoreEvent.draw);
                 break;
 
             case eCardState.tableau:
@@ -200,6 +201,7 @@ public class Prospector : MonoBehaviour
                 tableau.Remove(cd);
                 MoveToTarget(cd);
                 SetTableauFaces();
+                ScoreManager.EVENT(eScoreEvent.mine);
                 break;
                 
         }
@@ -230,11 +232,13 @@ public class Prospector : MonoBehaviour
     {
         if (won)
         {
-            print("Game Over. You Won! :)");
+            // print("Game Over. You Won! :)");
+            ScoreManager.EVENT(eScoreEvent.gameWin);
         }
         else
         {
-            print("Game Over. You Lost. :(");
+            //print("Game Over. You Lost. :(");
+            ScoreManager.EVENT(eScoreEvent.gameLoss);
         }
         SceneManager.LoadScene("_Prospector_Scene_0");
     }
